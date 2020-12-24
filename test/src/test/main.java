2636a2192d160b717b364b392d924888256562d3
unknown <alexkelsoo@gmail.com>
2020-12-23 19:10:30 -0800

    second commit

---
+++ test/src/test/main.java
@@ -0,0 +1,23 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Kumar is a bitch");
+		System.out.println("Everything in java is an object except primitive data types");
+		String string = " IS NOT A PRIMITIVE DATA TYPE";
+		boolean whatever = false; //true or false is boolean
+		Boolean bigBoolean = true;
+		
+		if(!whatever) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("false");
+
+		}
+		
+		}
+
+}
